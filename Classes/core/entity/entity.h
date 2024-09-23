634ad5e062a8afd9858d324f384131ca666bb52b
snylonue <snylonue@gmail.com>
2024-09-23 10:09:19 +0000

    add BuffMixin

--- Classes/core/entity/entity.h
+++ Classes/core/entity/entity.h
@@ -2,6 +2,7 @@
 #define TOWERDEFENCE_CORE_ENTITY_ENTITY
 
 #include <cstdint>
+#include <vector>
 
 namespace towerdefence {
 namespace core {
@@ -10,6 +11,17 @@ struct GridRef;
 
 struct AttackMixin {
     int32_t realized_attack = 0;
+};
+
+struct Buff {
+    int32_t attack_speed = 0;
+    double speed = 0;
+};
+
+struct BuffMixin {
+    std::vector<Buff> buffs;
+
+    void add_buff(Buff b) { buffs.push_back(b); }
 };
 
 struct Entity {
@@ -29,7 +41,7 @@ struct EnemyInfo {
     int32_t speed = 0;
 };
 
-struct Enemy : Entity, AttackMixin {
+struct Enemy : Entity, AttackMixin, BuffMixin {
     virtual EnemyInfo info() const = 0;
 };
 
@@ -39,11 +51,11 @@ struct TowerInfo {
     int32_t attack = 0;
     int32_t cost = 0;
     int32_t deploy_interval = 0;
-    int32_t attack_interval = 0;
+    int32_t attack_interval = 0; // actual_attack_attack_speed
     AttackType attack_type;
 };
 
-struct Tower : Entity, AttackMixin {
+struct Tower : Entity, AttackMixin, BuffMixin {
     virtual TowerInfo info() const = 0;
 
     void increase_attack(int32_t atk) { realized_attack += atk; }
