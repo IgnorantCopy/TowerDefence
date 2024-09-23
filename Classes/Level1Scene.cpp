b467a91818fb6d60940601cbcadc9cd3d7742b19
lichang-jin <3517179979@qq.com>
2024-09-23 17:07:31 +0800

    update

---
+++ Classes/Level1Scene.cpp
@@ -0,0 +1,65 @@
+#include "Level1Scene.h"
+#include "SelectLevelScene.h"
+#include "Gif/GIFMovie.h"
+#include "Gif/CacheGif.h"
+#include "Gif/InstantGif.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+
+Scene* Level1Scene::createScene()
+{
+    return Level1Scene::create();
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+bool Level1Scene::init()
+{
+    if ( !Scene::init() ){
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    auto closeItem = MenuItemImage::create(
+            "CloseNormal.png",
+            "CloseSelected.png",
+            CC_CALLBACK_1(Level1Scene::menuCloseCallback, this));
+    if (closeItem == nullptr ||
+        closeItem->getContentSize().width <= 0 ||
+        closeItem->getContentSize().height <= 0) {
+        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
+    } else {
+        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
+        float y = origin.y + closeItem->getContentSize().height/2;
+        closeItem->setPosition(Vec2(x,y));
+    }
+
+    auto background = Sprite::create("images/sellect_background.jpg",Rect(0,0,2500,1500));
+    if(background == nullptr) {
+        problemLoading("'images/sellect_background.jpg'");
+    } else {
+        background->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
+        this->addChild(background, 0)
+
+    return true;
+}
+
+void Level1Scene::menuCloseCallback(cocos2d::Ref *pSender)
+{
+    //Close the cocos2d-x game scene and quit the application
+    Director::getInstance()->end();
+
+    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
+
+    //EventCustom customEndEvent("game_scene_close_event");
+    //_eventDispatcher->dispatchEvent(&customEndEvent);
+
+}
