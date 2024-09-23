2c42ee028304f278348bbe7422ea13584349a437
snylonue <snylonue@gmail.com>
2024-09-23 08:39:25 +0000

    add GridRef::with_radix

--- Classes/core/map.h
+++ Classes/core/map.h
@@ -51,6 +51,12 @@ struct Map {
     GridRef get_ref(size_t row, size_t column);
 };
 
+static size_t absdiff(size_t x, size_t y) { return (x > y) ? x - y : y - x; }
+
+static size_t l1_dis(size_t x1, size_t y1, size_t x2, size_t y2) {
+    return absdiff(x1, x2) + absdiff(y1, y2);
+}
+
 struct GridRef {
     Map &map;
     Grid &grid;
@@ -61,6 +67,21 @@ struct GridRef {
     explicit GridRef(Map &m, size_t row_, size_t column_)
         : map(m), grid(m.grids[m.shape.get_index(row_, column_)]), row(row_),
           column(column_) {}
+
+    // Returns points whose distance between self <= radix
+    std::vector<GridRef> with_radix(size_t radix) {
+        std::vector<GridRef> res;
+        // todo: optimize to O(radix) algorithm
+        for (size_t i = 0; i < map.shape.height; ++i) {
+            for (size_t j = 0; j < map.shape.width; ++j) {
+                if (l1_dis(row, column, i, j) <= radix) {
+                    res.emplace_back(map, i, j);
+                }
+            }
+        }
+
+        return res;
+    }
 };
 
 } // namespace core
