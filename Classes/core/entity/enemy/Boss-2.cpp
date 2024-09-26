16b86291efae9f7f6b7e046b347d9aea46acc235
snylonue <snylonue@gmail.com>
2024-09-26 13:29:37 +0000

    more fixes of c++20 features

--- Classes/core/entity/enemy/Boss-2.cpp
+++ Classes/core/entity/enemy/Boss-2.cpp
@@ -7,7 +7,7 @@ namespace towerdefence {
         void Boss2::on_tick(GridRef g) {
             for (auto &grid : g.map.grids) {
                 grid.with_tower([this](std::unique_ptr<Tower> &tower) {
-                    tower->add_buff(this->id, {.attack_speed = -50, .silent = true});
+                    tower->add_buff(this->id, Buff::attack_speed(-50) & Buff::silent(true));
                 });
             }
         }
--- Classes/core/entity/enemy/Speed-up.cpp
+++ Classes/core/entity/enemy/Speed-up.cpp
@@ -7,7 +7,7 @@ namespace towerdefence {
         void SpeedUp::on_tick(GridRef g) {
             for (auto grid : g.with_radix(1, linf_dis)) {
                 grid.grid.with_enemy([this](Enemy &enemy) {
-                    enemy.add_buff(this->id, {.speed = 0.5});
+                    enemy.add_buff(this->id, Buff::speed(0.5));
                 });
             }
         }
