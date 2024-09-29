09c77fe628bad81f90061458eaa368866e1c2752
snylonue <snylonue@gmail.com>
2024-09-29 13:15:13 +0000

    build doc

--- flake.nix
+++ flake.nix
@@ -19,6 +19,43 @@
           just
         ];
       };
+
+      packages.x86_64-linux = {
+        doc = let
+          docs = [
+            "introduction"
+            "branch"
+            "enemy"
+            "level-1"
+            "level-2"
+            "level-3"
+            "logic"
+            "tower"
+          ];
+          tex = pkgs.texlive.combine {
+            inherit (pkgs.texlive) scheme-minimal latex-bin latexmk ctex;
+          };
+        in pkgs.stdenvNoCC.mkDerivation rec {
+          name = "towerdefence-doc";
+          src = ./docs;
+          buildInputs = [ pkgs.coreutils tex ];
+          phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            export XDG_CACHE_HOME="$(mktemp -d)";
+            mkdir -p .cache/texmf-var
+          '' + pkgs.lib.strings.concatMapStrings (d: ''
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                latexmk -interaction=nonstopmode -pdfxe \
+                ${d}.tex
+          '') docs;
+          installPhase = ''
+            mkdir -p $out
+          '' + pkgs.lib.concatMapStrings (d: ''
+            cp ${d}.pdf $out/
+          '') docs;
+        };
+      };
     };
 }
 
