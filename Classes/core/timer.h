f1b35358319d8cfa1026dee5edd18d990410b539
snylonue <snylonue@gmail.com>
2024-10-02 02:48:36 +0000

    refactor timer
    - rename
    - fix use of c++20 features

--- Classes/core/timer.h
+++ Classes/core/timer.h
@@ -12,7 +12,7 @@ template <class... Ts> struct overloaded : Ts... {
 
 template <class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-struct Clock {
+struct Timer {
     struct Period {
         uint32_t period = 1;
         uint32_t start = 0;
@@ -25,38 +25,38 @@ struct Clock {
 
     std::variant<Period, Duration> clk;
 
-    constexpr static Clock period(uint32_t period, uint32_t start) {
-        return {.clk = Period{.period = period, .start = start}};
+    constexpr static Timer period(uint32_t period, uint32_t start) {
+        return {Period{.period = period, .start = start}};
     }
 
-    constexpr static Clock duration(uint32_t duration, uint32_t start) {
-        return {.clk = Duration{.duration = duration, .start = start}};
+    constexpr static Timer duration(uint32_t duration, uint32_t start) {
+        return {Duration{.duration = duration, .start = start}};
     }
 };
 
-struct Timer {
+struct Clock {
     uint32_t elapased_ = 0;
 
     void on_tick() { elapased_ += 1; }
 
-    bool is_triggered(Clock clk) const {
+    bool is_triggered(Timer clk) const {
         return std::visit(
-            overloaded{[this](const Clock::Period &p) {
+            overloaded{[this](const Timer::Period &p) {
                            return (elapased_ - p.start) % p.period == 0;
                        },
-                       [this](const Clock::Duration &d) {
+                       [this](const Timer::Duration &d) {
                            return d.duration == elapased_ - d.start;
                        }},
             clk.clk);
     }
 
     // return a clock that can be notified after `duration`
-    Clock after(uint32_t duration) const {
-        return Clock::duration(duration, elapased_);
+    Timer with_duration(uint32_t duration) const {
+        return Timer::duration(duration, elapased_);
     }
 
     // return a clock that can be continously notified after `p`
-    Clock with_period(uint32_t p) const { return Clock::period(p, elapased_); }
+    Timer with_period(uint32_t p) const { return Timer::period(p, elapased_); }
 };
 
 } // namespace towerdefence::core::timer
