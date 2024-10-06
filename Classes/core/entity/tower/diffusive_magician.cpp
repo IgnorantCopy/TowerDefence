f32a17cdcf80abc5dc872ea646bcd8d307e543b8
UbiquinoneUQ <1162144082@qq.com>
2024-10-06 20:02:07 +0800

    finish skill of diffusiveMagician

--- Classes/core/entity/tower/diffusive_magician.cpp
+++ Classes/core/entity/tower/diffusive_magician.cpp
@@ -4,7 +4,18 @@
 namespace towerdefence {
     namespace core {
 
-        void diffusiveMagician::on_tick(GridRef g) {}
+        diffusiveMagician::diffusiveMagician(id::Id id, const timer::Clock &clk)
+                : Tower(id), release_skill_(clk.with_period_sec(20)) {}
+
+        void diffusiveMagician::on_tick(GridRef g) {
+            this->update_buff(g.clock());
+
+            if (g.clock().is_triggered(release_skill_)) {
+                this->add_buff_in({this->id, Buff::DEFAULT},
+                                  Buff::attack_speed(25),
+                                  g.clock().with_duration_sec(10));
+            }
+        }
 
     } // namespace core
 } // namespace towerdefence
