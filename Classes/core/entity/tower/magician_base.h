b99c2cfa19e1d9815976a7267a430bbaed346a1a
UbiquinoneUQ <1162144082@qq.com>
2024-10-06 19:43:20 +0800

    finish skill of magicianBase

--- Classes/core/entity/tower/magician_base.h
+++ Classes/core/entity/tower/magician_base.h
@@ -1,21 +1,27 @@
 #ifndef TOWERDEFENCE_MAGICIAN_BASE_H
 #define TOWERDEFENCE_MAGICIAN_BASE_H
 
+#include "../../id.h"
 #include "../entity.h"
+#include <unordered_set>
 
-namespace towerdefence {
-    namespace core {
+namespace towerdefence::core {
 
-        struct magicianBase final : Tower {
+    struct Map;
 
-            TowerInfo info() const override {
-                return { 450, 8, 0, 18, AttackType::Magic };
-            }
+    struct magicianBase final : Tower {
+        timer::Timer release_skill_;
+        std::unordered_set<id::Id> has_buff_;
 
-            void on_tick(GridRef g) override;
-        };
-    } // namespace core
-} // namespace towerdefence
+        magicianBase(id::Id id, const timer::Clock & clk);
+        magicianBase() = delete;
+
+        TowerInfo info() const override { return { 400, 9, 0, 15, AttackType::Magic }; }
+
+        void on_tick(GridRef g) override;
+    };
+} // namespace towerdefence::core
+
 
 
 
