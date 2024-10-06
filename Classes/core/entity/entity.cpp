eccc0d6bdacdf9014c9d11989740bc200928d81f
UbiquinoneUQ <1162144082@qq.com>
2024-10-06 18:12:28 +0800

    remove Entity::on_attack

--- Classes/core/entity/entity.cpp
+++ Classes/core/entity/entity.cpp
@@ -6,7 +6,6 @@ namespace towerdefence::core {
 void Entity::on_death(GridRef g) {}
 void Entity::on_tick(GridRef g) {}
 void Entity::on_hit(GridRef g) {}
-void Entity::on_attack(GridRef g) {}
 
 void Enemy::on_tick(GridRef g) {
     this->update_buff(g.clock());
