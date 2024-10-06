8dbec3249958c7ba1d19be54cb849c5c9dbbcad3
UbiquinoneUQ <1162144082@qq.com>
2024-10-06 16:19:25 +0800

    finish skill of archer

--- Classes/core/entity/tower/archer.h
+++ Classes/core/entity/tower/archer.h
@@ -1,21 +1,27 @@
 #ifndef TOWERDEFENCE_ARCHER_H
 #define TOWERDEFENCE_ARCHER_H
 
+#include "../../id.h"
 #include "../entity.h"
+#include <unordered_set>
 
-namespace towerdefence {
-    namespace core {
+namespace towerdefence::core {
 
-        struct archer final : Tower {
+    struct Map;
 
-            TowerInfo info() const override {
-                return { 2400, 18, 0, 10, AttackType::Physics };
-            }
+    struct archer final : Tower {
+        timer::Timer release_skill_;
+        std::unordered_set<id::Id> has_buff_;
 
-            void on_tick(GridRef g) override;
-        };
-    } // namespace core
-} // namespace towerdefence
+        archer(id::Id id, const timer::Clock & clk);
+        archer() = delete;
+
+        TowerInfo info() const override { return { 2400, 18, 0, 10, AttackType::Physics }; }
+
+        void on_tick(GridRef g) override;
+    };
+} // namespace towerdefence::core
+
 
 
 
