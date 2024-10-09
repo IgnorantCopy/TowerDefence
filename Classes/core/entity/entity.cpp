4bd3757e34b71cfcf4f79db38fd9dd51b13129ac
UbiquinoneUQ <1162144082@qq.com>
2024-10-09 21:31:29 +0800

    finish attack logic in Tower::on_tick

--- Classes/core/entity/entity.cpp
+++ Classes/core/entity/entity.cpp
@@ -27,18 +27,26 @@ void Enemy::on_tick(GridRef g) {
 
 void Tower::on_tick(GridRef g) {
     this->update_buff(g.clock());
+
     auto grids = g.with_radius(status().attack_radius_, linf_dis);
     auto enemy_grid = std::min_element(grids.begin(),grids.end(),[](const GridRef &a,const GridRef &b){
         auto a1 = std::min_element(a.grid.enemies.begin(),a.grid.enemies.end(),[](std::unique_ptr<Enemy>& x, std::unique_ptr<Enemy>& y){
             return x->get_distance() < y->get_distance();
         });
-        auto a2 = std::min_element(b.grid.enemies.begin(),b.grid.enemies.end(),[](std::unique_ptr<Enemy>& x, std::unique_ptr<Enemy>& y){
+        auto b1 = std::min_element(b.grid.enemies.begin(),b.grid.enemies.end(),[](std::unique_ptr<Enemy>& x, std::unique_ptr<Enemy>& y){
             return x->get_distance() < y->get_distance();
         });
-        return (*a1)->get_distance()<(*a2)->get_distance();
+        return (*a1)->get_distance()<(*b1)->get_distance();
     });
-    if(enemy!=grids.end()){
-        enemy
+    if(enemy_grid!=grids.end()){
+        auto target_enemy=std::min_element(enemy_grid->grid.enemies.begin(),enemy_grid->grid.enemies.end(),[](std::unique_ptr<Enemy>& x, std::unique_ptr<Enemy>& y){
+            return x->get_distance() < y->get_distance();
+        });
+        (*target_enemy)->increase_attack(status().attack_,status().attack_type_);
+        auto buffs=get_all_buff();
+        if(buffs.real_attack_>0){
+            (*target_enemy)->increase_attack(status().attack_*buffs.real_attack_,AttackType::Real);
+        }
     }
 }
 
