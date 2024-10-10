64575c7352052f2caec0486849b4a03748cb73e6
UbiquinoneUQ <1162144082@qq.com>
2024-10-10 19:42:51 +0800

    finish skill of highspeedarcherplus

--- Classes/core/entity/tower/highspeed_archor_plus.cpp
+++ Classes/core/entity/tower/highspeed_archor_plus.cpp
@@ -3,8 +3,28 @@
 
 namespace towerdefence {
     namespace core {
+        HighspeedArcherPlus::HighspeedArcherPlus(id::Id id, const timer::Clock &clk)
+                : Tower(id), release_skill_(clk.with_period_sec(5)) {}
+
         void HighspeedArcherPlus::on_tick(GridRef g) {
             Tower::on_tick(g);
+            auto grids = g.with_radius(this->status().attack_radius_, linf_dis);
+            auto enemy_grid = get_enemy_grid(*this,grids);
+            if(enemy_grid!=grids.end()){
+                if(skill){
+                    this->add_buff({this->id, Buff::DEFAULT},
+                                   Buff::attack(0.60));
+                    single_attack(*this,*enemy_grid);
+                    this->remove_buff_from(this->id);
+                    skill = false;
+                }else{
+                    single_attack(*this,*enemy_grid);
+                }
+            }
+
+            if (g.clock().is_triggered(release_skill_)) {
+                skill = true;
+            }
         }
     } // namespace core
 } // namespace towerdefence
