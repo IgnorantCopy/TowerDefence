87f122bc9520e85c53a9415f6a269e7508d00a8e
UbiquinoneUQ <1162144082@qq.com>
2024-10-11 16:35:50 +0800

    update bomberPlus

--- Classes/core/entity/tower/bomber_plus.h
+++ Classes/core/entity/tower/bomber_plus.h
@@ -1,20 +1,27 @@
 #ifndef TOWERDEFENCE_BOMBER_PLUS_H
 #define TOWERDEFENCE_BOMBER_PLUS_H
 
+#include "../../id.h"
 #include "../entity.h"
+#include <unordered_set>
 
-namespace towerdefence {
-    namespace core {
+namespace towerdefence::core {
+    struct Bomber final : Tower {
+        timer::Timer release_skill_;
+        std::unordered_set<id::Id> has_buff_;
 
-        struct BomberPlus final : Tower {
+        Bomber(id::Id id, const timer::Clock & clk);
+        Bomber() = delete;
 
-            static constexpr TowerInfo INFO { 2000, 20, 0, 20, 4, AttackType::Physics };
-            TowerInfo info() const override { return INFO; }
+        static constexpr TowerInfo INFO { 2000, 20, 0, 20, 4, AttackType::Physics };
+        TowerInfo info() const override { return INFO; }
 
-            void on_tick(GridRef g) override;
-        };
-    } // namespace core
-} // namespace towerdefence
+        bool skill = false;
+
+        void on_tick(GridRef g) override;
+    };
+} // namespace towerdefence::core
+
 
 
 
