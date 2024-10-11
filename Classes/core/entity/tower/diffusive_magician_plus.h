b9b107d3ba3bf2aa8f3f3790c5d74be698ddbf7e
snylonue <snylonue@gmail.com>
2024-10-11 12:58:26 +0000

    finish skill of diffusive magician plus

--- Classes/core/entity/tower/diffusive_magician_plus.h
+++ Classes/core/entity/tower/diffusive_magician_plus.h
@@ -4,18 +4,21 @@
 #include "../entity.h"
 
 namespace towerdefence {
-    namespace core {
+namespace core {
 
-        struct DiffusiveMagicianPlus final : Tower {
+struct DiffusiveMagicianPlus final : Tower {
+    timer::Timer attack_;
+    timer::CallbackTimer<DiffusiveMagicianPlus&, GridRef> timeouts_;
 
-            static constexpr TowerInfo INFO { 2000, 21, 0, 20, 4, AttackType::Magic };
-            TowerInfo info() const override { return INFO; }
+    DiffusiveMagicianPlus(id::Id id, const timer::Clock &clk)
+        : Tower{id}, attack_{clk.with_period_sec(INFO.attack_interval_)} {}
 
-            void on_tick(GridRef g) override;
-       };
-    } // namespace core
+    static constexpr TowerInfo INFO{2000, 21, 0, 20, 4, AttackType::Magic};
+    TowerInfo info() const override { return INFO; }
+
+    void on_tick(GridRef g) override;
+};
+} // namespace core
 } // namespace towerdefence
 
-
-
-#endif //TOWERDEFENCE_DIFFUSIVE_MAGICIAN_PLUS_H
+#endif // TOWERDEFENCE_DIFFUSIVE_MAGICIAN_PLUS_H
