e70ed7c6d3abc96dd1b17494ce1a472c460cdcaf
UbiquinoneUQ <1162144082@qq.com>
2024-10-11 21:37:35 +0800

    finish skill of AggressiveMagician and its plus

--- Classes/core/entity/tower/aggressive_magician.cpp
+++ Classes/core/entity/tower/aggressive_magician.cpp
@@ -1,10 +1,46 @@
 #include "aggressive_magician.h"
 #include "../../map.h"
+#include <stdexcept>
 
 namespace towerdefence {
     namespace core {
+        AggressiveMagician::AggressiveMagician(id::Id id, const timer::Clock &clk)
+                : Tower(id), release_skill_(clk.with_period_sec(70)),
+                  attack_(clk.with_period_sec(INFO.attack_interval_)) {}
         void AggressiveMagician::on_tick(GridRef g) {
             Tower::on_tick(g);
+            auto status = this->status();
+            auto &clk = g.clock();
+            if (clk.is_triggered(this->attack_)){
+                this->attack_ = clk.never();
+                for (auto grid : g.with_radius(status.attack_radius_, linf_dis)) {
+                    grid.current().with_tower(
+                            [this, &clk = g.clock()](std::unique_ptr<Tower> &tower) {
+                                tower->add_buff({this->id, Buff::DEFAULT},
+                                                Buff::attack(0.20));
+                            });
+                }
+            }
+            if (clk.is_triggered(release_skill_)) {
+                this->release_skill_ = clk.never(); // stop release_skill
+                for (auto grid : g.with_radius(status.attack_radius_, linf_dis)) {
+                    grid.current().with_tower(
+                            [this, &clk = g.clock()](std::unique_ptr<Tower> &tower) {
+                                tower->add_buff({this->id, Buff::DEFAULT},
+                                                Buff::attack(0.50));
+                            });
+                }
+            }
+        }
+
+        void AggressiveMagician::on_death(GridRef g) {
+            for (auto tower_id : has_buff_) {
+                try {
+                    auto &tower = g.map.get_tower_by_id(tower_id);
+                    tower.remove_buff_from(id);
+                } catch (const std::out_of_range &) {
+                }
+            }
         }
     } // namespace core
 } // namespace towerdefence
--- Classes/core/entity/tower/aggressive_magician_plus.h
+++ Classes/core/entity/tower/aggressive_magician_plus.h
@@ -2,16 +2,24 @@
 #define TOWERDEFENCE_AGGRESSIVE_MAGICIAN_PLUS_H
 
 #include "../entity.h"
+#include <unordered_set>
 
 namespace towerdefence {
     namespace core {
 
         struct AggressiveMagicianPlus final : Tower {
+            timer::Timer release_skill_; // timer to release skill
+            timer::Timer attack_;        // timer to do normal attack
+
+            std::unordered_set<id::Id> has_buff_;
+
+            AggressiveMagicianPlus(id::Id id, const timer::Clock &clk);
 
             static constexpr TowerInfo INFO { 650, 14, 0, 16, 3, AttackType::Magic };
             TowerInfo info() const override { return INFO; }
 
             void on_tick(GridRef g) override;
+            void on_death(GridRef g) override;
         };
     } // namespace core
 } // namespace towerdefence
