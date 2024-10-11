0f15838d0e051204e900ac36b779f4d44508862c
UbiquinoneUQ <1162144082@qq.com>
2024-10-11 14:31:17 +0800

    finish skill of bomber

--- Classes/core/entity/tower/bomber.cpp
+++ Classes/core/entity/tower/bomber.cpp
@@ -3,8 +3,36 @@
 
 namespace towerdefence {
     namespace core {
+        Bomber::Bomber(id::Id id, const timer::Clock &clk)
+                : Tower(id), release_skill_(clk.with_period_sec(8)) {}
+
         void Bomber::on_tick(GridRef g) {
             Tower::on_tick(g);
+            size_t sputter_radius = 1;
+            auto grids = g.with_radius(this->status().attack_radius_, linf_dis);
+            auto enemy_grid = get_enemy_grid(*this,grids);
+            if(enemy_grid!=grids.end()){
+                if(skill){
+                    sputter_radius = 2;
+                    for (auto ref : enemy_grid->with_radius(sputter_radius, l1_dis)) {
+                        ref.grid.with_enemy([this](Enemy &enemy) {
+                            enemy.increase_attack(this->status().attack_,this->status().attack_type_);
+                        });
+                    }
+                    sputter_radius = 1;
+                    skill = false;
+                }else{
+                    for (auto ref : enemy_grid->with_radius(sputter_radius, l1_dis)) {
+                        ref.grid.with_enemy([this](Enemy &enemy) {
+                            enemy.increase_attack(this->status().attack_,this->status().attack_type_);
+                        });
+                    }
+                }
+            }
+
+            if (g.clock().is_triggered(release_skill_)) {
+                skill = true;
+            }
         }
     } // namespace core
 } // namespace towerdefence
