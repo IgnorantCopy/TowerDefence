IgnorantCopy/TowerDefence

commit 7c18a5bf3f42401df093a94cca1c3ef60fe9888a

refactor: create attack in Tower

Classes/core/entity/entity.h
@@ -40,6 +40,10 @@ struct AttackMixin {
     int32_t realized_attack_ = 0;
 };
 
+struct NormalAttackMixin {
+    timer::Timer attack_ = timer::Timer::never();
+};
+
 #define BUFF_CONSTRUCTOR(type, name)                                           \
     static constexpr Buff name(type name) {                                    \
         Buff b;                                                                \
@@ -267,8 +271,10 @@ struct TowerInfo {
     }
 };
 
-struct Tower : Entity, AttackMixin, BuffMixin, IdMixin {
-    Tower(id::Id id) : IdMixin{id} {}
+struct Tower : Entity, AttackMixin, BuffMixin, IdMixin, NormalAttackMixin {
+    Tower(id::Id id, const timer::Clock &clk) : IdMixin{id} {
+        this->reset_attack_timer(clk);
+    }
 
     virtual TowerInfo info() const = 0;
 
@@ -287,13 +293,18 @@ struct Tower : Entity, AttackMixin, BuffMixin, IdMixin {
     TowerType type;
 
     void on_tick(GridRef g) override;
+
+    void reset_attack_timer(const timer::Clock &clk) {
+        this->attack_ = clk.with_period_sec(this->status().attack_interval_);
+    }
 };
 
 [[deprecated("use grid_of_nearest_enemy instead")]] auto
 get_enemy_grid(Tower &,
                std::vector<GridRef> &) -> std::vector<GridRef>::iterator;
 
-auto grid_of_nearest_enemy(std::vector<GridRef> &grids) -> std::vector<GridRef>::iterator;
+auto grid_of_nearest_enemy(std::vector<GridRef> &grids)
+    -> std::vector<GridRef>::iterator;
 
 void single_attack(Tower &, GridRef);
 

Classes/core/entity/tower/archer.cpp
@@ -4,7 +4,7 @@
 namespace towerdefence {
     namespace core {
         Archer::Archer(id::Id id, const timer::Clock &clk)
-                : Tower(id), release_skill_(clk.with_period_sec(20)) {}
+                : Tower(id, clk), release_skill_(clk.with_period_sec(20)) {}
 
         void Archer::on_tick(GridRef g) {
             Tower::on_tick(g);

Classes/core/entity/tower/archer_base.cpp
@@ -5,7 +5,7 @@ namespace towerdefence {
     namespace core {
 
         ArcherBase::ArcherBase(id::Id id, const timer::Clock &clk)
-                : Tower(id), release_skill_(clk.with_period_sec(20)) {}
+                : Tower(id, clk), release_skill_(clk.with_period_sec(20)) {}
 
         void ArcherBase::on_tick(GridRef g) {
             Tower::on_tick(g);

Classes/core/entity/tower/archer_plus.cpp
@@ -5,7 +5,7 @@ namespace towerdefence {
     namespace core {
 
         ArcherPlus::ArcherPlus(id::Id id, const timer::Clock &clk)
-                : Tower(id), release_skill_(clk.with_period_sec(45)) {}
+                : Tower(id, clk), release_skill_(clk.with_period_sec(45)) {}
 
         void ArcherPlus::on_tick(GridRef g) {
             Tower::on_tick(g);

Classes/core/entity/tower/bomber.cpp
@@ -4,7 +4,7 @@
 namespace towerdefence {
     namespace core {
         Bomber::Bomber(id::Id id, const timer::Clock &clk)
-                : Tower(id), release_skill_(clk.with_period_sec(8)) {}
+                : Tower(id, clk), release_skill_(clk.with_period_sec(8)) {}
 
         void Bomber::on_tick(GridRef g) {
             Tower::on_tick(g);

Classes/core/entity/tower/bomber_plus.cpp
@@ -4,8 +4,7 @@
 namespace towerdefence {
 namespace core {
 BomberPlus::BomberPlus(id::Id id, const timer::Clock &clk)
-    : Tower(id), release_skill_(clk.with_period_sec(20)),
-      attack_(clk.with_period_sec(INFO.attack_interval_)) {}
+    : Tower(id, clk), release_skill_(clk.with_period_sec(20)) {}
 
 void BomberPlus::on_tick(GridRef g) {
     Tower::on_tick(g);

Classes/core/entity/tower/bomber_plus.h
@@ -8,7 +8,6 @@
 namespace towerdefence::core {
 struct BomberPlus final : Tower {
     timer::Timer release_skill_; // timer to release skill
-    timer::Timer attack_;        // timer to do normal attack
 
     // store functions that will be executed after certain time
     timer::CallbackTimer<BomberPlus &, GridRef> timeouts_;

Classes/core/entity/tower/core_magician.cpp
@@ -4,7 +4,7 @@
 namespace towerdefence {
     namespace core {
         CoreMagician::CoreMagician(id::Id id, const timer::Clock &clk)
-                : Tower(id), release_skill_(clk.with_period_sec(8)) {}
+                : Tower(id, clk), release_skill_(clk.with_period_sec(8)) {}
 
         void CoreMagician::on_tick(GridRef g) {
             Tower::on_tick(g);

commit 7a4be33d2a5b1fc6309d4b45674703863a9c3b3c

fix call of virtual function in constructor
fixes: 7c18a5b

Classes/core/entity/entity.h
@@ -228,8 +228,10 @@ struct EnemyInfo {
     int32_t speed_ = 0;
     EnemyType enemy_type_;
 
-    constexpr EnemyInfo(int32_t health, Defence defence, int32_t speed, EnemyType enemy_type)
-        : health_(health), defence_(defence), speed_(speed), enemy_type_(enemy_type){}
+    constexpr EnemyInfo(int32_t health, Defence defence, int32_t speed,
+                        EnemyType enemy_type)
+        : health_(health), defence_(defence), speed_(speed),
+          enemy_type_(enemy_type) {}
 };
 
 struct Enemy : Entity, AttackMixin, BuffMixin, IdMixin, RouteMixin {
@@ -273,7 +275,7 @@ struct TowerInfo {
                         AttackType attack_type, TowerType tower_type)
         : attack_(attack), cost_(cost), deploy_interval_(deploy_interval),
           attack_interval_(attack_interval), attack_radius_(attack_radius),
-          attack_type_(attack_type), tower_type_(tower_type){}
+          attack_type_(attack_type), tower_type_(tower_type) {}
 
     TowerInfo with_attack_radius(size_t r) const noexcept {
         auto copied = *this;
@@ -289,9 +291,7 @@ struct TowerInfo {
 };
 
 struct Tower : Entity, AttackMixin, BuffMixin, IdMixin, NormalAttackMixin {
-    Tower(id::Id id, const timer::Clock &clk) : IdMixin{id} {
-        this->reset_attack_timer(clk);
-    }
+    Tower(id::Id id, const timer::Clock &clk) : IdMixin{id} {}
 
     Tower(Tower &&) = delete;
 
@@ -316,8 +316,7 @@ struct Tower : Entity, AttackMixin, BuffMixin, IdMixin, NormalAttackMixin {
     }
 };
 
-template <class Self, class G = GridRef, class... Args>
-struct TimeOutMixin {
+template <class Self, class G = GridRef, class... Args> struct TimeOutMixin {
     timer::CallbackTimer<Self &, G, Args...> timeouts_;
 
     void stop_timer_for(timer::Timer &timer, uint32_t d,
@@ -374,6 +373,8 @@ struct TowerFactoryBase {
     virtual std::unique_ptr<Tower> construct(id::Id id,
                                              const timer::Clock &clk) = 0;
     virtual TowerInfo info() const = 0;
+
+    virtual ~TowerFactoryBase() = default;
 };
 
 template <class T> struct TowerFactory : TowerFactoryBase {
@@ -385,13 +386,18 @@ template <class T> struct TowerFactory : TowerFactoryBase {
 template <class T>
 std::unique_ptr<Tower> TowerFactory<T>::construct(id::Id id,
                                                   const timer::Clock &clk) {
+    std::unique_ptr<Tower> res;
     if constexpr (std::is_constructible_v<T, id::Id, const timer::Clock &>) {
-        return std::make_unique<T>(id, clk);
+        res = std::make_unique<T>(id, clk);
     } else if constexpr (std::is_constructible_v<T, id::Id>) {
-        return std::make_unique<T>(id);
+        res = std::make_unique<T>(id);
     } else {
         static_assert(false, "Unsupported type");
     }
+
+    res->reset_attack_timer(clk);
+
+    return res;
 }
 
 template <class T> TowerInfo TowerFactory<T>::info() const { return T::INFO; }