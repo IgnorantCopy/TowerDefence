64fc000d48191262f3aafb764c9bf72da980f59e
UbiquinoneUQ <1162144082@qq.com>
2024-10-12 16:52:49 +0800

    add some todos

--- Classes/core/entity/tower/special_magician.h
+++ Classes/core/entity/tower/special_magician.h
@@ -7,6 +7,12 @@ namespace towerdefence {
     namespace core {
 
         struct SpecialMagician final : Tower {
+            timer::Timer release_skill_; // timer to release skill
+
+            // store functions that will be executed after certain time
+            timer::CallbackTimer<SpecialMagician &, GridRef> timeouts_;
+
+            SpecialMagician(id::Id id, const timer::Clock &clk);
 
             static constexpr TowerInfo INFO { 1400, 14, 0, 30, 2, AttackType::Magic, TowerType::SpecialMagician};
             TowerInfo info() const override { return INFO; }
