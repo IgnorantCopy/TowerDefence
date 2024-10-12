bb5de00dee845a28c95e4cd17121f02ddf7d32db
snylonue <snylonue@gmail.com>
2024-10-12 11:22:57 +0000

    finish skill of special magician

--- Classes/core/entity/tower/special_magician.cpp
+++ Classes/core/entity/tower/special_magician.cpp
@@ -1,10 +1,42 @@
 #include "special_magician.h"
 #include "../../map.h"
+#include <algorithm>
 
 namespace towerdefence {
-    namespace core {
-        void SpecialMagician::on_tick(GridRef g) {
-            Tower::on_tick(g);
+namespace core {
+void SpecialMagician::on_tick(GridRef g) {
+    Tower::on_tick(g);
+
+    auto &clk = g.clock();
+    auto status = this->status();
+
+    if (clk.is_triggered(this->attack_)) {
+        auto grids = g.with_radius(status.attack_radius_, linf_dis);
+        if (auto it = grid_of_nearest_enemy(grids); it != grids.end()) {
+            auto grid = *it;
+            if (this->has_skill) {
+                grid.attack_enemies_in_radius(
+                    status.with_attack_radius(1).with_attack(status.attack_ *
+                                                             1.5),
+                    linf_dis);
+                this->has_skill = false;
+            } else {
+                single_attack(*this, grid);
+            }
+            this->is_attacking = true;
+            this->silence_count = 0;
+        } else {
+            this->is_attacking = false;
         }
-    } // namespace core
+    }
+
+    if (clk.is_triggered(this->update_silence_count) && !this->is_attacking) {
+        this->silence_count = std::min(this->silence_count + 1, 3zu);
+    }
+
+    if (clk.is_triggered(this->update_silence_count)) {
+        this->has_skill = true;
+    }
+}
+} // namespace core
 } // namespace towerdefence
--- Classes/core/entity/tower/special_magician.h
+++ Classes/core/entity/tower/special_magician.h
@@ -4,18 +4,34 @@
 #include "../entity.h"
 
 namespace towerdefence {
-    namespace core {
+namespace core {
 
-        struct SpecialMagician final : Tower {
+struct SpecialMagician final : Tower {
 
-            static constexpr TowerInfo INFO { 1400, 14, 0, 30, 2, AttackType::Magic };
-            TowerInfo info() const override { return INFO; }
+    bool is_attacking = false;
+    size_t silence_count = 0;
+    timer::Timer update_silence_count;
+    timer::Timer release_skill;
+    bool has_skill = false;
 
-            void on_tick(GridRef g) override;
-        };
-    } // namespace core
+    SpecialMagician(id::Id id, const timer::Clock &clk)
+        : Tower(id, clk), release_skill(clk.with_period_sec(10)),
+          update_silence_count{clk.with_period_sec(3)} {}
+
+    static constexpr TowerInfo INFO{1400, 14, 0, 30, 2, AttackType::Magic};
+    TowerInfo info() const override { return INFO; }
+
+    TowerInfo status() const override {
+        auto status = Tower::status();
+
+        status.attack_ *= 1 + this->silence_count * 0.5;
+
+        return status;
+    }
+
+    void on_tick(GridRef g) override;
+};
+} // namespace core
 } // namespace towerdefence
 
-
-
-#endif //TOWERDEFENCE_SPECIAL_MAGICIAN_H
+#endif // TOWERDEFENCE_SPECIAL_MAGICIAN_H
