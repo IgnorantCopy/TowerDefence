f1b949006d5f4003f3ee5ecc6086fffd25fdd9b6
UbiquinoneUQ <1162144082@qq.com>
2024-10-13 20:50:56 +0800

    fix fix

--- Classes/core/entity/entity.cpp
+++ Classes/core/entity/entity.cpp
@@ -36,7 +36,7 @@ void Enemy::on_tick(GridRef g) {
             auto ny = route::ssize(g.column) + dy;
             assert(nx >= 0 && nx < g.map.shape.height_ && ny >= 0 && ny < g.map.shape.width_);
             g.map.move_enemy_to(this->id, nx, ny);
-            g.on_enemy_move(*this, {route::ssize(g.row), route::ssize(g.column)}, {nx, ny});
+            g.on_enemy_move(*this, std::make_pair(route::ssize(g.row), route::ssize(g.column)), std::make_pair(nx, ny));
         } catch (const std::out_of_range &) {
             g.map.reached_end(this->id);
         }
