f7465853a1a1337e26d5bc0c18a157eb041238f1
UbiquinoneUQ <1162144082@qq.com>
2024-10-20 23:59:18 +0800

    game patches: strengthen core_magician_plus

--- Classes/core/entity/tower/core_magician_plus.h
+++ Classes/core/entity/tower/core_magician_plus.h
@@ -13,7 +13,7 @@ namespace towerdefence::core {
         CoreMagicianPlus(id::Id id, const timer::Clock & clk);
         CoreMagicianPlus() = delete;
 
-        static constexpr TowerInfo INFO { 1200, 16, 0, 16, 2, AttackType::Magic, TowerType::CoreMagicianPlus};
+        static constexpr TowerInfo INFO { 1500, 16, 0, 16, 2, AttackType::Magic, TowerType::CoreMagicianPlus};
         TowerInfo info() const override { return INFO; }
 
         void on_tick(GridRef g) override;
