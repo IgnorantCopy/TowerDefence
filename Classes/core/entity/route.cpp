d354da39e275e1e37f7b0472d5dc8443c7bef62f
snylonue <snylonue@gmail.com>
2024-10-20 08:10:36 +0000

    add missing file

---
+++ Classes/core/entity/route.cpp
@@ -0,0 +1,14 @@
+#include "route.h"
+
+#include <iostream>
+
+namespace towerdefence::core::route {
+std::tuple<ssize, ssize> Route::next_direction() try {
+    std::cout << std::format("{}", std::format("{}", *this));
+    auto [x, y] = diffs.at(pos);
+    pos += 1;
+    return {x, y};
+} catch (const std::out_of_range &) {
+    throw reached_end();
+}
+} // namespace towerdefence::core::route
