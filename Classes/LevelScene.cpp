5fa207b17b1a8976b5ea8f8b508be4ecb6dbd328
lichang-jin <3517179979@qq.com>
2024-10-21 10:25:11 +0800

    update Animation

--- Classes/LevelScene.cpp
+++ Classes/LevelScene.cpp
@@ -1337,7 +1337,7 @@ void LevelScene::createMap(int level) {
                          Dir[D], Dir[R], Dir[R], Dir[U], Dir[U], Dir[L],
                          Dir[L], Dir[L], Dir[L], Dir[L]})};
         enemyFirstDir = {R, L, L, L, L, R};
-        enemyCreateTime = {1.0,  11.0,  12.0,  15.0,  16.0,  17.0,  20.0,
+        enemyCreateTime = {10.0,  11.0,  12.0,  15.0,  16.0,  17.0,  20.0,
                            22.0,  24.0,  30.0,  33.0,  36.0,  39.0,  45.0,
                            46.0,  47.0,  55.0,  57.0,  59.0,  65.0,  68.0,
                            71.0,  72.0,  77.0,  80.0,  85.0,  95.0,  101.0,
